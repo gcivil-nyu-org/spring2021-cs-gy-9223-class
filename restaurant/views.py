c24f0de15135908ac209fa62c91af9f2ea31d117
YuhanChen8789 <50022448+YuhanChen8789@users.noreply.github.com>
2021-04-12 23:28:56 +0800

    Improvement (#119)
    * user page allow to edit review
    
    * feedback form
    
    * Update views.py
    
    * Update tests.py

--- restaurant/views.py
+++ restaurant/views.py
@@ -12,7 +12,7 @@ import random
 from .models import Restaurant, FAQ
 
 from .forms import (
-    # QuestionnaireForm,
+    QuestionnaireForm,
     SearchFilterForm,
 )
 from user.forms import (
@@ -61,7 +61,6 @@ logger = logging.getLogger(__name__)
 
 
 def get_restaurant_profile(request, restaurant_id):
-
     if request.method == "POST" and "content" in request.POST:
         url = reverse("restaurant:profile", args=[restaurant_id])
         if request.user.is_authenticated:
@@ -72,6 +71,16 @@ def get_restaurant_profile(request, restaurant_id):
             return HttpResponseRedirect(url)
         else:
             messages.error(request, "Please login before making review")
+            return HttpResponseRedirect(url)
+
+    if request.method == "POST" and "employee_mask" in request.POST:
+        form = QuestionnaireForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, "Thank you for your feedback!", extra_tags="feedback"
+            )
+            url = reverse("restaurant:profile", args=[restaurant_id])
             return HttpResponseRedirect(url)
 
     try:
@@ -296,6 +305,7 @@ def edit_review(request, restaurant_id, review_id, action, source):
         review = Review.objects.get(id=review_id)
         review.rating = request.POST.get("rating")
         review.content = request.POST.get("content")
+        review.hidden = False
         review.save()
         messages.success(request, "success")
     if source == "restaurant":
